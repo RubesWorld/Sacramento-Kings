8e0fcde79e87a560b8f79d9037281021da9a8826
RubesWorld <hi@rubesworld.live>
2021-05-13 20:52:04 -0700

    Added structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./app.scss";
 
 function App() {
+  const [countdownDate, setCountdownDate] = useState(
+    new Date("04/26/2006").getTime()
+  );
+  const [state, setState] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+  });
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>
+            The <span>Sacramento Kings</span> have NOT made the playoffs in:
+          </h1>
+          <h2>15 years</h2>
+          <h2>3 months</h2>
+          <h2>25 days</h2>
+          <h2>24 hours</h2>
+          <h2>14 minutes</h2>
+        </div>
       </header>
     </div>
   );
